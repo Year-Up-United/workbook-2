715457b184d5cc5a26d92f3502a71c6101a71a95
thejaylajones <janeissaj@gmail.com>
2025-04-18 11:25:58 -0400

    changes updated

---
+++ TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // system is built so that customers can reserve tickets for specific date
+        // the user will need to input their full name, date of show & # of tickets needed
+
+
+
+
+
+
+
+
+
+
+    }
+}
